4477914d4b4397f626c06eae1ac9c15259aacfc3
cullenmustone <81482794+cullenmustone@users.noreply.github.com>
2021-11-28 19:10:38 -0500

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,10 +1,9 @@
-##This program will be run as the master for the project
 import RPi.GPIO as gpio
 import time
 import os
 import glob
 
-##sets up the pinout for the GPIO pins for the motor driver
+
 def init():
  gpio.setmode(gpio.BCM)
  gpio.setup(17, gpio.OUT)
@@ -13,11 +12,11 @@ def init():
  gpio.setup(24, gpio.OUT)
 
 sensor=21
-gpio.setmode(GPIO.BCM)
-gpio.setup(sensor, GPIO.IN)
+gpio.setmode(gpio.BCM)
+gpio.setup(sensor, gpio.IN)
 
 def moisture(sensor):
-    if GPIO.input(sensor):
+    if gpio.input(sensor):
         print("no water")
         def forward():
          gpio.output(17, True)
@@ -29,9 +28,11 @@ def moisture(sensor):
         print("water")
 
 
-GPIO.add_event_detect(sensor, GPIO.BOTH, bouncetime=100)
-GPIO.add_event_callback(sensor, moisture)
+gpio.add_event_detect(sensor, gpio.BOTH, bouncetime=100)
+gpio.add_event_callback(sensor, moisture)
 
-##Main loop
 while True:
- time.sleep(1)
+  time.sleep(1)
+
+
+
