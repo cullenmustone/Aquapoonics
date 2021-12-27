07500ba66a7f48629f050d8330e79182f48beaba
cullenmustone <81482794+cullenmustone@users.noreply.github.com>
2021-12-27 15:22:55 -0500

    Update pumpTester.py

--- pumpTester.py
+++ pumpTester.py
@@ -1,29 +1,60 @@
 import time
+
 import RPi.GPIO as gpio
 
 
+
+
+
 #declares pins motors
+
 def init():
+
     gpio.setmode(gpio.BCM)
+
     gpio.setup(17, gpio.OUT)
+
     gpio.setup(22, gpio.OUT)
+
     gpio.setup(23, gpio.OUT)
+
     gpio.setup(24, gpio.OUT)
 
+
+
 #declares function to drive
-def forward(sec):
+
+def reverse(sec):
+
     init()
-    gpio.output(17, True)
-    gpio.output(22, False)
-    gpio.output(23, True)
-    gpio.output(24, False)
+
+    gpio.output(17, False)
+
+    gpio.output(22, True)
+
+    gpio.output(23, False)
+
+    gpio.output(24, True)
+
     time.sleep(sec)
+
     gpio.cleanup()
 
 
 
+
+
+
+
 while True:
+
     for i in range(10, 0, -1):
+
         print(i)
+
         time.sleep(1)
-        forward(4)
+
+    reverse (10)
+    
+    
+    
